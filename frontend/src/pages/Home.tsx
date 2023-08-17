af2a89889ec475172596db8fd393b9abec5e7f44
mark <a.markdexter@gmail.com>
2023-08-17 17:37:51 +0800

    adding the initial layout

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,37 @@
+import { useEffect, useState } from "react"
+import axios from 'axios'
 
 const Home = () => {
-  return (
-   <div className="flex align-center justify-start m-8">
-    Home
-      <div className="grid grid-cols-2">
+  const [workouts, setWorkouts] = useState([])
 
+  useEffect(() => {
+    axios.get('http://localhost:4000/api/workouts')
+      .then((res) => {
+        const result = res.data
+
+        setWorkouts(result)
+      })
+      .catch((err) => console.warn(err))
+    
+  }, [])
+
+  return (
+   <div className="flex align-center justify-start flex-col m-8">
+      <div className="grid grid-cols-2 p-5">
+          <div className="workouts p-5">
+            { workouts?.length ? (
+                  workouts?.map((workout: any) => (
+                    <p key={workout._id}>
+                      { workout.title }
+                    </p>
+                  ))
+            ) : (
+              <div>
+                Loading...
+              </div>
+            )
+           }
+          </div>
       </div>
     </div>
   )
