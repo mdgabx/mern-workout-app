905639bd2a2a5b87a7c6ab8085ae473c3027b076
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2023-10-08 16:57:38 +0800

    hashing password

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,6 +5,17 @@ const loginUser = async(req, res) => {
 }
 
 const signupUser = async(req, res) => {
+    const { email, password } = req.body
+
+    try {
+        const user = await User.signup(email, password)
+
+        res.status(200).json({email, user})
+        
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+
     res.json({ msg: "Signup" })
 }
 
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const Schema = mongoose.Schema
-
 
 const userSchema = new Schema({
     email: {
@@ -14,5 +14,30 @@ const userSchema = new Schema({
         required: true
     }
 })
+
+// static signup method
+userSchema.statics.signup = async function(email, password) {
+    
+    const exists = await this.findOne({
+        email
+    })
+
+    if (exists) {
+        throw Error('Email already in use')
+    }
+
+    // password
+
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    const user = await this.create({
+        email,
+        password: hash
+    })
+
+    return user
+}
+
 
 module.exports = mongoose.model('User', userSchema)
