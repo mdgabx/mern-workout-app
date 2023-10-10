1be6f4457e3ba0038480ec1520129f1b7fe9dbfa
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2023-10-10 10:21:23 +0800

    adding jsonwebtoken

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,9 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+const createToken = (id) => {
+    return jwt.sign({ id }, process.env.SECRET, { expiresIn: '3d' })
+}
 
 const loginUser = async(req, res) => {
     res.json({ msg: "Login"  })
@@ -10,7 +15,10 @@ const signupUser = async(req, res) => {
     try {
         const user = await User.signup(email, password)
 
-        res.status(200).json({email, user})
+        // create token 
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
 
     } catch (error) {
         res.status(400).json({ error: error.message })
