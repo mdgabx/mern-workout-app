52be3e1f86f0ca05dfe1a8ba2f02cf29d5731e28
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2023-10-13 21:32:25 +0800

    adding the tailwind css styles for the form

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,14 +4,14 @@ const Navbar = () => {
   return (
     <div>
         <header className="p-8 bg-cyan-800">
-            <div className="container">
+            <div className="container w-full flex justify-between">
                 <Link to="/" >
                     <h1 className="text-3xl text-white font-poppins font-bold">Workout App</h1>
                 </Link>
-                <nav>
-                  <div>
-                    <Link to="/login">Login</Link>
-                    <Link to="/signup">Signup</Link>
+                <nav className="flex items-center justify-end">
+                  <div className="flex gap-2 font-poppins text-white">
+                    <Link to="/login" className="hover:text-gray-500">Login</Link>
+                    <Link to="/signup" className="hover:text-gray-500">Signup</Link>
                   </div>
                 </nav>
             </div>
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -17,21 +17,27 @@ const Login = () => {
     }
 
     return (
-        <>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <h3>Sign up</h3>
+        <div className="flex mt-10 items-center justify-center">
+            <form onSubmit={handleSubmit(onSubmit)} className="bg-white w-2/5 mt-10 rounded-lg p-6 font-poppins">
+                <h3 className="font-bold text-2xl mb-5">Sign up</h3>
 
-                <label>Email:</label>
-                <input placeholder='Email address' {...register('email') } />
-                <p>{errors.email?.message}</p>
+                <div className="flex flex-col my-3">    
+                <label className="font-bold">Email:</label>
+                <input className="my-1 p-2 border-2 border-gray-400 rounded-md" placeholder='Email address' {...register('email') } />
+                <p >{errors.email?.message}</p>
+                </div>
+              
 
-                <label>Password:</label>
-                <input placeholder='Password' {...register('password')} /> 
-                <p>{errors.password?.message}</p>
+                <div className="flex flex-col my-3">
+                <label className="font-bold">Password:</label>
+                <input className="my-1 p-2 border-2 border-gray-400 rounded-md" placeholder='Password' {...register('password')} /> 
+                <p className="text-red">{errors.password?.message}</p>
+                </div>
+              
 
-                <button type="submit">Submit</button>
+                <button className="text-center m-auto bg-cyan-800 p-2 my-3 rounded-md text-white" type="submit">Submit</button>
             </form>
-        </>
+        </div>
 
     )
 }
